0281d0d89e86b7d6b2724252620d17d865ee049a
Thomasjkeel <thomasjames.keel@googlemail.com>
2020-05-18 17:30:34 +0100

    clean up

--- home_chores.py
+++ home_chores.py
@@ -25,7 +25,8 @@ SEND_EMAILS = True
 EMAIL_SENT_TODAY = False
 SCROLL_SPEED = (0.06)
 
-RANDOM_EVENTS = ['Board Game', 'Book Club', 'Garden Time', 'Movie Time', 'You Decide', 'Craft Club', 'Party Game']
+RANDOM_EVENTS = ['Board Game', 'Book Club', 'Garden Time',
+                 'Movie Time', 'You Decide', 'Craft Club', 'Party Game']
 
 
 r = (255, 0, 0)
@@ -40,13 +41,12 @@ TEXT_COLOUR = t
 CURRENT = ''
 LAST = 's'
 
+
 def watch_pi():
     while True:
         global CURRENT, LAST, SEND_EMAILS
         for event in sense.stick.get_events():
             CURRENT = event.direction
-            print(event.direction, event.action)
-            print(CURRENT, LAST)
             if event.action == 'pressed':
                 if CURRENT != LAST:
                     if event.direction == 'up':
@@ -65,7 +65,8 @@ def watch_pi():
                                            text_colour=TEXT_COLOUR, scroll_speed=(0.03))
                     elif event.direction == 'middle':
                         print('getting random event...')
-                        rand_event = RANDOM_EVENTS[random.randint(0, len(RANDOM_EVENTS)-1)]
+                        rand_event = RANDOM_EVENTS[random.randint(
+                            0, len(RANDOM_EVENTS)-1)]
                         sense.show_message('Random event is: %s' % (rand_event), back_colour=BACK_COLOUR,
                                            text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
                     else:
@@ -74,12 +75,14 @@ def watch_pi():
                 else:
                     ## double clicks ##
                     if event.direction == 'up':
-                        sense.show_message('powering off...', text_colour=r, scroll_speed=SCROLL_SPEED)
+                        sense.show_message(
+                            'powering off...', text_colour=r, scroll_speed=SCROLL_SPEED)
                         time.sleep(10)
                         os._exit(1)
                         # subprocess.Popen(['sudo' 'shutdown', '-h', 'now'])
                     elif event.direction == 'down':
-                        sense.show_message('For chores press down', back_colour=BACK_COLOUR, text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
+                        sense.show_message('For chores press down', back_colour=BACK_COLOUR,
+                                           text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
                     elif event.direction == 'left':
                         print('getting facts...')
                         fact = collect_facts.collect_facts()
@@ -87,7 +90,7 @@ def watch_pi():
                             fact, back_colour=BACK_COLOUR, text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
                     elif event.direction == 'right':
                         sense.show_message('toggling emails. set to: %s ' %
-                              (not SEND_EMAILS), back_colour=BACK_COLOUR, text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED )
+                                           (not SEND_EMAILS), back_colour=BACK_COLOUR, text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
                         SEND_EMAILS = not SEND_EMAILS
                     elif event.direction == 'middle':
                         print('dance baby...')
@@ -95,18 +98,19 @@ def watch_pi():
                     else:
                         pass
                     LAST = 'a'
-                
+
 
 def make_email(sender, name, email_add, current_date):
-    _, all_chores = chores.get_chores()  
-    daily_chores = ("".join(['<li>%s</li>']*len(all_chores['daily'][name[0]]))) % tuple(all_chores['daily'][name[0]])
-    weekly_chores = ("".join(['<li>%s</li>']*len(all_chores['weekly'][name[0]]))) % tuple(all_chores['weekly'][name[0]])
+    _, all_chores = chores.get_chores()
+    daily_chores = ("".join(['<li>%s</li>']*len(all_chores['daily']
+                                                [name[0]]))) % tuple(all_chores['daily'][name[0]])
+    weekly_chores = ("".join(['<li>%s</li>']*len(all_chores['weekly']
+                                                 [name[0]]))) % tuple(all_chores['weekly'][name[0]])
     emailSubject = "Chores for %s" % (current_date)
     _, week_ending = chores.get_current_week_range()
     emailContent = "Hello %s! <br><br> Your Daily Chores for today are: <br> <ul> %s </ul> <br><br> Chores which will \
         need to be completed by %s are: <br> <ul> %s </ul> <br><br> Raspberry Pi out." % (name, daily_chores, str(week_ending), weekly_chores)
-    print(emailContent)
-    # sender.sendmail('thomasjames.keel@googlemail.com', emailSubject, emailContent)
+    sender.sendmail(name, emailSubject, emailContent)
     print(name, 'sent!')
     return
 
@@ -115,16 +119,16 @@ def distribute_emails():
     global SEND_EMAILS, EMAIL_SENT_TODAY
     while True:
         if SEND_EMAILS:
-            print('sending?')
             current_time = time.localtime()
             if current_time.tm_hour == EMAIL_TIME_HOUR and current_time.tm_min == EMAIL_TIME_MINUTE and not EMAIL_SENT_TODAY:
-                print('actually sending')
-                sense.show_message('sending emails',  back_colour=BACK_COLOUR, text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
+                sense.show_message('sending emails',  back_colour=BACK_COLOUR,
+                                   text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
                 sender = emailer.Emailer()
                 for name, email_add in EMAIL_ADDRESSES.items():
-                    print(name, email_add)
-                current_date = datetime.datetime.now().strftime('%d %b')
-                make_email(sender, name, email_add, current_date)
+                    current_date = datetime.datetime.now().strftime('%d %b')
+                    make_email(sender, name, email_add, current_date)
+                sense.show_message('all emails sent',  back_colour=BACK_COLOUR,
+                                   text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
                 EMAIL_SENT_TODAY = True
             elif current_time.tm_hour == 1 and current_time.tm_min == 0:
                 EMAIL_SENT_TODAY = False
@@ -139,7 +143,7 @@ def get_weather(sense):
     humidity = round(humidity, 1)
     pressure = sense.get_pressure()
     pressure = round(pressure, 1)
-    sense.show_message("Temperature: %s.C  Humidity: %s%%  Pressure: %s mb" % (temp, humidity, pressure),
+    sense.show_message("Temperature: %s.C  Humidity: %s%%  Pressure: %smb" % (temp, humidity, pressure),
                        back_colour=BACK_COLOUR, text_colour=TEXT_COLOUR, scroll_speed=SCROLL_SPEED)
     return
 
@@ -163,12 +167,9 @@ def dance_baby(sense):
 if __name__ == "__main__":
     t1 = threading.Thread(target=watch_pi)
     t2 = threading.Thread(target=distribute_emails)
-    # t3 = threading.Thread(target=end_program)
     t1.setDaemon(True)
     t2.setDaemon(True)
-    # t3.setDaemon(True)
     t1.start()
     t2.start()
-    # t3.start()
     while True:
         pass
